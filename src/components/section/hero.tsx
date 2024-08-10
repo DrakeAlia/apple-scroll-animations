ac52cc1c16b0eb520cdf4a56fabda8306fc12bd8
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-09 17:17:13 -0700

    added hero scroll animations

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import { Button } from "../button";
 import { Container } from "../container";
+import { useScroll, useTransform, motion } from "framer-motion";
 import Image from "next/image";
 
 export const Hero = () => {
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: videoContainerRef,
+    offset: ["start start", "end end"],
+  });
+
+  const opacity = useTransform(scrollYProgress, [0, 0.7, 1], [1, 1, 0]);
+
   return (
     <div className="bg-background relative h-[300vh] text-white">
-      <div className="absolute left-0 -top-[--header-height] h-[200vh] w-full ">
+      <motion.div
+        style={{ opacity }}
+        ref={videoContainerRef}
+        className="absolute left-0 -top-[--header-height] h-[200vh] w-full "
+      >
         {/* <img src="/images/napoleon.png" alt="Napoleon image" /> */}
         <Image
           src="/images/napoleon.png"
@@ -15,16 +30,28 @@ export const Hero = () => {
           height={1080}
           className="sticky top-0 h-screen w-full object-cover"
         />
-      </div>
-      <Container className="relative z-10 flex min-h-[--hero-height] flex-col justify-end items-start pb-7">
-        <h1 className="text-5xl font-bold mb-10">
-          All Apple Originals. <br />
-          Only on Apple TV+.
-        </h1>
-        <Button className="mb-16" size="large">
-          Stream now
-        </Button>
-        <p className="font-semibold">Watch on the 📺 app.</p>
+      </motion.div>
+      <Container className="relative z-10 h-[--hero-height] pb-7">
+        <motion.div
+          className="flex h-full flex-col items-start justify-end"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          whileHover="visible"
+          exit="hidden"
+          animate="hidden"
+          viewport={{ amount: 0.98 }}
+        >
+          <h1 className="text-5xl font-bold mb-10">
+            All Apple Originals. <br />
+            Only on Apple TV+.
+          </h1>
+          <Button className="mb-16" size="large">
+            Stream now
+          </Button>
+          <p className="font-semibold">Watch on the 📺 app.</p>
+        </motion.div>
       </Container>
     </div>
   );
