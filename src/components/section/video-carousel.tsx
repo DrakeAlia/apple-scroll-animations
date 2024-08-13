1f27223d72ca6d9a55badb6a7b33d08a14b5626a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-13 16:52:57 -0700

    setup video carousel with images

--- src/components/section/video-carousel.tsx
+++ src/components/section/video-carousel.tsx
@@ -1,13 +1,62 @@
+import Image from "next/image";
 import React from "react";
-import { Container } from "../container";
+import { motion } from "framer-motion";
+import { Movie, movies, randomMoviesSet1, randomMoviesSet2 } from "@/movies";
 
 export const VideoCarousel = () => {
   return (
     <div className="bg-background">
-      <div>3 large videos</div>
+      <div className="flex gap-5 mb-5 overflow-clip">
+        <div className="w-[60vw] shrink-0 rounded-2xl overflow-clip">
+          <Image
+            src={movies[0].poster}
+            alt={movies[0].name}
+            width={1920}
+            height={1080}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="w-[60vw] shrink-0 rounded-2xl overflow-clip">
+          <Image
+            src={movies[1].poster}
+            alt={movies[1].name}
+            width={1920}
+            height={1080}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="w-[60vw] shrink-0 rounded-2xl overflow-clip">
+          <Image
+            src={movies[2].poster}
+            alt={movies[2].name}
+            width={1920}
+            height={1080}
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
 
-      <div>carousel</div>
-      <div>carousel</div>
+      <SmallVideoCarousel movies={randomMoviesSet1} />
+      <SmallVideoCarousel movies={randomMoviesSet2} />
+    </div>
+  );
+};
+
+const SmallVideoCarousel = ({ movies }: { movies: Movie[] }) => {
+  return (
+    <div className="flex gap-3 overflow-clip">
+      {movies.map((movie, index) => (
+        // <img key={`${movie.name} -${index}`} src={movie.poster} alt={movie.name} />
+        <div className="w-[23vw] shrink-0" key={`${movie.name} -${index}`}>
+          <Image
+            src={movie.poster}
+            alt={movie.name}
+            width={1920}
+            height={1080}
+            className="w-full h-full object-cover rounded-xl"
+          />
+        </div>
+      ))}
     </div>
   );
 };
