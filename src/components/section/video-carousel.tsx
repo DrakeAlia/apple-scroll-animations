123b2a25d3a3a56ff974907832f0620081ab3a3f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-14 14:50:00 -0700

    added in fade in for images from the left and right of the carousel

--- src/components/section/video-carousel.tsx
+++ src/components/section/video-carousel.tsx
@@ -1,18 +1,41 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { Movie, movies, randomMoviesSet1, randomMoviesSet2 } from "@/movies";
+import { useWindowSize } from "react-use";
 
 export const VideoCarousel = () => {
+  const { width, height } = useWindowSize();
   const carouselWrapperRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: carouselWrapperRef,
     offset: ["start start", "end start"],
   });
+  const maxiumScale = useMemo(() => {
+    const windowYRatio = height / width;
+    const xScale = 1.66667;
+    const yScale = xScale * (16 / 9) * windowYRatio;
+    return Math.max(xScale, yScale);
+  }, [width, height]);
+  const scale = useTransform(
+    scrollYProgress,
+    [0.3, 0.5, 0.66],
+    [maxiumScale * 1.1, maxiumScale, 1]
+  );
 
-  const scale = useTransform(scrollYProgress, [0.3, 0.5, 0.66], [3, 2.5, 1]);
+  const postersOpacity = useTransform(scrollYProgress, [0.64, 0.66], [0, 1]);
+  const posterTranslateXLeft = useTransform(
+    scrollYProgress,
+    [0.64, 0.66],
+    [-20, 0]
+  );
+  const posterTranslateXRight = useTransform(
+    scrollYProgress,
+    [0.64, 0.66],
+    [20, 0]
+  );
 
   return (
     <div className="bg-background pb-8">
@@ -22,7 +45,10 @@ export const VideoCarousel = () => {
       >
         <div className="sticky h-screen top-0 flex items-center">
           <div className="relative left-1/2 mb-5 flex -translate-x-1/2 gap-5">
-            <div className="aspect-video w-[60vw] shrink-0 overflow-clip rounded-2xl">
+            <motion.div
+              style={{ scale: postersOpacity, x: posterTranslateXLeft }}
+              className="aspect-video w-[60vw] shrink-0 overflow-clip rounded-2xl"
+            >
               <Image
                 src={movies[0].poster}
                 alt={movies[0].name}
@@ -30,7 +56,7 @@ export const VideoCarousel = () => {
                 height={1080}
                 className="h-full w-full object-cover"
               />
-            </div>
+            </motion.div>
             <motion.div
               style={{ scale }}
               className="aspect-video w-[60vw] shrink-0 overflow-clip rounded-2xl"
@@ -43,7 +69,10 @@ export const VideoCarousel = () => {
                 className="h-full w-full object-cover"
               />
             </motion.div>
-            <div className="aspect-video w-[60vw] shrink-0 overflow-clip rounded-2xl">
+            <motion.div
+              style={{ scale: postersOpacity, x: posterTranslateXRight }}
+              className="aspect-video w-[60vw] shrink-0 overflow-clip rounded-2xl"
+            >
               <Image
                 src={movies[2].poster}
                 alt={movies[2].name}
@@ -51,7 +80,7 @@ export const VideoCarousel = () => {
                 height={1080}
                 className="h-full w-full object-cover"
               />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
